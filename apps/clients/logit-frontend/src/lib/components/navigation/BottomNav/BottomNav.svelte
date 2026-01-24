f2e19edd54fefff520a90355c3e0b69d9bb8ca78
Martin Melody <martinmelody.work@gmail.com>
2026-01-24 21:15:42 +0000

    adding lucide

--- apps/clients/logit-frontend/src/lib/components/navigation/BottomNav/BottomNav.svelte
+++ apps/clients/logit-frontend/src/lib/components/navigation/BottomNav/BottomNav.svelte
@@ -1,10 +1,24 @@
+<script>
+  import { ChartColumnDecreasing, Dumbbell, House } from "lucide-svelte";
+</script>
+
 <nav
   aria-label="Bottom navigation"
   class="flex h-14 items-center justify-around"
 >
-  <div class="flex flex-column justify-between">
+  <div class="flex flex-col">
     <ul>
-      <li></li>
+      <li>
+        <House />
+      </li>
+
+      <li>
+        <Dumbbell />
+      </li>
+
+      <li>
+        <ChartColumnDecreasing />
+      </li>
     </ul>
   </div>
 </nav>
